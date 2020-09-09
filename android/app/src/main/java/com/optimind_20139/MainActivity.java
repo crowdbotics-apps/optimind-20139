6c9c6d17c109ae365a3c913c51243c205dbfa11f
Crowdbotics <team@crowdbotics.com>
2020-09-09 13:46:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/optimind_20139/MainActivity.java
@@ -0,0 +1,15 @@
+package com.optimind_20139;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "optimind_20139";
+    }
+}
